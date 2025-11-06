78261face7410b0b96a02884a16cde546e1f4a57
jfmsantana <jfmsantana10@gmail.com>
2025-11-06 19:08:55 -0300

    feature: Estrutura do Projeto criada

---
+++ backend/src/main/java/com/example/backend/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Perfil perfil;
+
+    public enum Perfil {
+        ALUNO,
+        PROFESSOR,
+        ADMIN
+    }
+}
